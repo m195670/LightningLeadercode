ef280fd28c377989bc14ddae56260c9eaa91be44
m195670 <49844775+m195670@users.noreply.github.com>
2019-04-21 18:16:03 -0400

    Add files via upload

---
+++ Flatlandscape.m
@@ -0,0 +1,22 @@
+
+function [Probabilities, ExpectedValue, Variance] = Flatlandscape(H)
+Probabilities = ones(1,H);
+%This initializes the vector associated with the probability values.
+E = ones(1,H);
+%This initializes a vector to make computing the expected value easier.
+E2 = ones(1,H);
+%This initializes a vector to make computing the expected value easier.
+for i = 0:(H-1)
+    P = nchoosek(H-1,i)*(1/2)^(H-1);
+    %This calculates the probability at the specific point
+    Probabilities(1,i+1) = P;
+    %Puts the calculated Probability into the vector that will be output
+    E(H-i)= (H-(i))*P;
+    %Calculates the expected value
+    E2(H-i) = ((H-(i))^2)*P;
+    %Used in the calculation for the variance
+end
+ExpectedValue = sum(E);
+%Final calculation for the expected value
+Variance = sum(E2) - (ExpectedValue)^2;
+%Final Calculation for the Variance
